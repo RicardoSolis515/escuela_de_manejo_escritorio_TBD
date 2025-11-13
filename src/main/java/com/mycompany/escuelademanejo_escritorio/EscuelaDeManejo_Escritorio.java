2f203dcb5bf51283cd05443bf1a20dc40bd74ec3
Ricardo Solis <rsolis@tecjerez.edu.mx>
2025-11-13 16:07:47 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/escuelademanejo_escritorio/EscuelaDeManejo_Escritorio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.escuelademanejo_escritorio;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EscuelaDeManejo_Escritorio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
